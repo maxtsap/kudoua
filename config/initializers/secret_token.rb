fb8dd10e25d9428b72ae3f1bdc480dd8ea85a880
Maxim Tsaplin <maxtsap@gmail.com>
2011-09-05 11:00:57 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kudi::Application.config.secret_token = '7134fe9283c9dcbd56703971d0abf2a740f42ced9525850e34cc95bd0cc418dfd885a82aba42274b30e244bda182a893251415df0998af3157b0aa85d323d840'
