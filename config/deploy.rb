6f443fa7f40f2c22f1d72bfa65399bbb06048815
Maxim Tsaplin <maxim.tsaplin@anahoret.com>
2012-01-11 20:58:03 +0200

    Fail.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # У вас должна быть настроена авторизация ssh по сертификатам
-require 'rvm/capistrano'
-require 'bundler/capistrano'
+# require 'rvm/capistrano'
+# require 'bundler/capistrano'
 
 set :application, "kudo-ua"
 
@@ -17,8 +17,8 @@ role :web, "lithium.locum.ru"   # Your HTTP server, Apache/etc
 role :app, "lithium.locum.ru"   # This may be the same as your `Web` server
 role :db,  "lithium.locum.ru", :primary => true # This is where Rails migrations will run
 
-set :bundle_without, [:development, :test]
-set :bundle_flags, "--deployment"
+# set :bundle_without, [:development, :test]
+# set :bundle_flags, "--deployment"
 
 # эта секция для того, чтобы вы не хранили доступ к базе в системе контроля версий. Поместите dayabase.yml в shared,
 # чтобы он копировался в нужный путь при каждом выкладывании новой версии кода
