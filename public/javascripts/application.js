3c6fb9c9eeba33ed65f21033111fcb2213588aea
Maxim Tsaplin <maxim.tsaplin@anahoret.com>
2012-02-06 20:32:04 +0200

    Make dropdown menu.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
   $("nav#menu ul li").hover(
       function () {
+        $("ul", this).css(
+          {
+            left: $(this).width() + "px",
+            top: $(this).height() + "px"
+          }
+        )
         $('>ul', this).slideDown(100);
       },
       function () {
