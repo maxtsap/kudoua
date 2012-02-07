c3e98a97f648a77dff6c3a57716be1d3b77bbbc8
Maxim Tsaplin <maxim.tsaplin@anahoret.com>
2012-02-07 21:26:57 +0200

    Fix banner 3.

--- vendor/engines/banners/app/controllers/admin/banners_controller.rb
+++ vendor/engines/banners/app/controllers/admin/banners_controller.rb
@@ -1,8 +1,7 @@
 module Admin
   class BannersController < Admin::BaseController
 
-    crudify :banner,
-            :title_attribute => 'link', :xhr_paging => true
+    crudify :banner
 
   end
 end
