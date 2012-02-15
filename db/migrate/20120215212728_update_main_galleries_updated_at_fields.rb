9841c062a53ba29d0b97bfd1f41a9462b2f94919
Maxim <maxim.tsaplin@anahoret.com>
2012-02-15 23:55:55 +0200

    Fix migration 2.

--- db/migrate/20120215212728_update_main_galleries_updated_at_fields.rb
+++ db/migrate/20120215212728_update_main_galleries_updated_at_fields.rb
@@ -1,6 +1,6 @@
 class UpdateMainGalleriesUpdatedAtFields < ActiveRecord::Migration
   def self.up
-    update("UPDATE main_galleries SET updated_at=#{Time.current.to_s(:db)}, created_at=#{Time.current.to_s(:db)}")
+    update("UPDATE main_galleries SET updated_at='#{Time.current.to_s(:db)}', created_at='#{Time.current.to_s(:db)}'")
   end
 
   def self.down
