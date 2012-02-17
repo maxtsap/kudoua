70a4fc4ce3ed6a6950a82bc4730e226a5858d9a1
Maxim Tsaplin <maxim.tsaplin@anahoret.com>
2012-02-17 11:07:18 +0200

    Fix application variable for staging.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,6 @@
 set :deploy_to, "/home/hosting_kudoua/projects/skudo"
 set :rails_env, "staging"
+set :application, "skudo"
 
 set :unicorn_conf, "/etc/unicorn/skudo.kudoua.rb"
 set :unicorn_pid, "/var/run/unicorn/skudo.kudoua.pid"
